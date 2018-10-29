1effbbdcfc4ecf09f103fb1424956f69d5b74698
yilian <liub@reed.edu>
2018-10-29 14:47:30 -0700

    On branch master  Changes to be committed: 	modified:   test_tree.cc
     Changes not staged for commit:
    	modified:   README.md
    modified:   tree.cc
    
     Untracked files:
    	a.out

--- test_tree.cc
+++ test_tree.cc
@@ -3,4 +3,14 @@
 
 int main()
 {
-	create_tree
+	tree_ptr_t root = create_tree(1, 10, nullptr, nullptr);
+	root->left_ = create_tree(2, 15, create_tree(4, 15, nullptr, nullptr), create_tree(5,10,nullptr,nullptr));
+	root->right_ = create_tree(3, 7, create_tree(6, 9, nullptr, nullptr), create_tree(7,1,nullptr,nullptr));
+	bool isIn =  isKeyInTree(root, 6);
+	std::cout << isIn;
+	std::string str = path_to(root, 6);
+	std::cout<<str;
+	std::cout<<(node_at(root, str)->value_);
+	destroy_tree(root);
+//	std::cout<<root->left_;
+}
